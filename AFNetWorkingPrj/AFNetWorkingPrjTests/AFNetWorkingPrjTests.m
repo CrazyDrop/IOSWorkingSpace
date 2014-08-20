556a7d0cc3f521f40aeb5b80cdb776523f80b650
Apple <apple@AppledeMacBook-Pro.local>
2014-08-20 11:19:19 +0800

    新建工程文件  完成网络请求封装

---
+++ AFNetWorkingPrj/AFNetWorkingPrjTests/AFNetWorkingPrjTests.m
@@ -0,0 +1,34 @@
+//
+//  AFNetWorkingPrjTests.m
+//  AFNetWorkingPrjTests
+//
+//  Created by Apple on 14-8-20.
+//  Copyright (c) 2014年 zhangchaoqun. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFNetWorkingPrjTests : XCTestCase
+
+@end
+
+@implementation AFNetWorkingPrjTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

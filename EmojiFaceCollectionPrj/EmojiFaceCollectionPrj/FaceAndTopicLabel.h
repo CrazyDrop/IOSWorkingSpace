4a493327594fe62adc4403023a283b5764474e21
CrazyDrop <wo160.160@163.com>
2014-08-30 17:25:24 +0800

    update test controller

---
+++ EmojiFaceCollectionPrj/EmojiFaceCollectionPrj/FaceAndTopicLabel.h
@@ -0,0 +1,13 @@
+//
+//  FaceAndTopicLabel.h
+//  EmojiFaceCollectionPrj
+//
+//  Created by zhangchaoqun on 14-8-29.
+//  Copyright (c) 2014年 zhangchaoqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceAndTopicLabel : UILabel
+
+@end
